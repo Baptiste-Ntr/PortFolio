2a7cf2dc5bdda8fe2299df82e44950c7ebe5bcba
Baptiste NAUTRE <baptiste.nautre@hotmail.fr>
2021-09-07 10:27:10 +0200

    test icon

--- JS/script.js
+++ JS/script.js
@@ -11,7 +11,7 @@ new Typewriter(AnimTXT, {
   .changeDelay(100)
   .typeString("Bonjour, <br>")
   .pauseFor(300)
-  .typeString("Voici mon portfolio.")
+  .typeString("Bienvenue sur mon portfolio.")
   .pause(1000)
   .deleteChars(10)
   .typeString("<a class='blue'> Portfolio</a>." )
